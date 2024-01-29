99b642f165e86b72407a40f99e62c53b9d139e33
adrian <adrian@fury.black>
2024-01-29 17:43:27 +0530

    commit

--- contracts/fu4-group/src/state.rs
+++ contracts/fu4-group/src/state.rs
@@ -1,16 +1,15 @@
-use cosmwasm_std::{Addr, Singleton, Bucket, Snapshot, ReadonlyStorage, StdResult};
+use cosmwasm_std::{Addr, ReadonlyStorage, StdResult, Storage, Snapshot, Map};
 use cw_controllers::{Admin, Hooks};
+use cw_storage_plus::{Item, Bucket, Strategy};
+
 use tg4::{MemberInfo, TOTAL_KEY};
 
 pub const ADMIN: Admin = Admin::new("admin");
 pub const HOOKS: Hooks = Hooks::new("tg4-hooks");
 
-pub const TOTAL: Singleton<u64> = Singleton::new(TOTAL_KEY);
+pub const TOTAL: Item<u64> = Item::new(TOTAL_KEY);
 
 pub const MEMBERS: Bucket<Addr, MemberInfo> = Bucket::new(tg4::MEMBERS_KEY);
-
-// Optional: If you need changelog functionality
-// pub const MEMBERS_CHANGELOG: Changelog = Changelog::new("members_changelog");
 
 impl ReadonlyStorage for Snapshot {
     fn get(&self, key: &[u8]) -> StdResult<Option<Vec<u8>>> {
@@ -25,8 +24,3 @@ impl ReadonlyStorage for Snapshot {
         ReadonlyStorage::prefix(self, prefix)
     }
 }
-
-// Uncomment the code below if you need to use `Changelog` (MEMBERS_CHANGELOG)
-
-// use cosmwasm_std::Changelog;
-// pub const MEMBERS_CHANGELOG: Changelog = Changelog::new("members_changelog");
